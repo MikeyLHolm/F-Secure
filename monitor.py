d044d4460d8d870c8252a2fcdc970a7048a42b29
Mikko Lindholm <mlindhol@c2r6p3.hive.fi>
2020-10-05 15:00:15 +0300

    added open protect and mkdir logs

--- monitor.py
+++ monitor.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-import os, requests
+import os, requests, sys
 from datetime import datetime
 
 class ResponseInfo(object):
-    def __init__(self, date_time, site, response_status_code, response_time):
+    def __init__(self, date_time, web_site, response_status_code, response_time):
         self.date_time = date_time
-        self.web_site = site
-        self.status_code = response_status_code
-        self.response_time = response_time
+        self.web_site = web_site
+        self.status_code = str(response_status_code)
+        self.response_time = str(response_time)
 
 blue = '\033[94m'
 endc = '\033[0m'
@@ -15,42 +15,50 @@ green = '\033[92m'
 red = '\033[91m'
 yellow = '\033[93m'
 
-
-def log_response(date_time_str, response, response_log, site):
+def get_response_object(date_time_str, response, web_site):
     response_status_code = response.status_code
     response_time = response.elapsed.total_seconds()
+    response_object = ResponseInfo(date_time_str, web_site, response_status_code, response_time)
+    return response_object
 
-    response_log.write(date_time_str)
-    response_log.write("\n" + site )
-    response_log.write("\nStatus code: " + str(response_status_code))
-    response_log.write("\nResponse time: " + str(response_time) + "\n\n")
-
-    response_object = ResponseInfo(date_time_str, site, response_status_code, response_time)
-    response_object
-    print(response_object.date_time)
+def log_response(response_log, response_object):
+    response_log.write(response_object.date_time + '\n')
+    response_log.write(response_object.web_site + '\n')
+    response_log.write("Status code: " + response_object.status_code + '\n')
+    response_log.write("Response time: " + response_object.response_time + "\n\n")
 
 # REMEMBER HTTP!
 def run_requests():
-    print('inside requests')
     web_sites = [
         'https://www.f-secure.com/fi',
         'https://www.hive.fi/en/',
         'https://www.python.org',
         'http://www.foobar.com/login'
     ]
+    if not os.path.isdir("logs/"):
+        print(yellow + 'Creating logs/' + endc)
+        os.mkdir("logs")
 
-    response_log = open(os.path.join("logs/", "monitor_log"), "a")
-    for site in web_sites:
-        response = requests.get(site)
+    file_name = os.path.join("logs/", "monitor_log")
+    try:
+        response_log = open(file_name, "a")
+    except OSError:
+        print (red + "Could not open/read file:" + file_name + endc)
+        sys.exit()
+
+    print(yellow + 'Running requests' + endc)
+    for web_site in web_sites:
         date_time_str = str(datetime.now())
-        log_response(date_time_str, response, response_log, site)
+        response = requests.get(web_site)
+        response_object = get_response_object(date_time_str, response, web_site)
+        log_response(response_log, response_object)
 
     response_log.close
 
 
 
 def main():
-    print(yellow + 'Running requests' + endc)
+    # add intervals
     run_requests()
     print(green + 'Requests done. Logs are located at logs/monitor_log' + endc)
 
